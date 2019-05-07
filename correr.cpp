66cb79669c1ec1316af74a87c9313e689fde296a
Gabriel2633 <40802589+Gabriel2633@users.noreply.github.com>
2019-05-07 07:47:30 -0500

    Add files via upload

--- correr.cpp
+++ correr.cpp
@@ -4,74 +4,63 @@
 using namespace std;
 
 
-void funcion(double rho,double ten,int N,double dn,int Nt, double dt,string filename);
+void funcion(double rho,double ten,int N,int Nt,string filename);
 
 int main()
 {
     
-    funcion(0.01,40.0,101,0.01,101,0.5,"datos.dat");
+    funcion(0.01,0.00002,101,600,"datos.dat");
     
     return 0;
 }
 
-void funcion(double rho,double ten,int N,double dn,int Nt, double dt,string filename)
+void funcion(double rho,double ten,int N,int Nt,string filename)
 {
     ofstream outfile;
     outfile.open(filename);
-    double c = pow((ten/rho),0.5);
+    double c = 0.5;// pero aca hay un error del libro pq c tiene que ser menor que C1
+    double dn=0.01,dt=0.01;
     double c1= dn/dt;
     double ratio=c*c/(c1*c1);
-        
-    double xi[3][N];    
-    for(int i=0; i<81;i++)
+    
+    double xi[3][N];
+    
+    
+    for(int i=0; i<N;i++)
     {
-        xi[0][i]=0.00125*i; 
+        xi[0][i]=sin(i*3.1416/(N-1));
     }
-    for(int i=81; i<N;i++)
-    {
-        xi[0][i]=0.1-0.005*(i-80); 
-    }
-   
+    
+    
     xi[0][0]=0.0;
     xi[0][N-1]=0.0;
-    
     xi[1][0]=0.0;
     xi[1][N-1]=0.0;
     
+    
     for(int i=1;i<N-1;i++)
     {
-        xi[1][i]=xi[0][i]+0.5*ratio*(xi[0][i+1]+xi[0][i-1]-2*xi[0][i]);     
+        xi[1][i]=xi[0][i]+0.5*ratio*(xi[0][i+1]+xi[0][i-1]-2*xi[0][i]);
     }
     
-    for(int i=0;i<N;i++)
-    {
-      outfile << xi[0][i] << " ";
-    }
-      outfile << "\n";
     
-    // para los t 
-    
-      for(int i=1;i<Nt;i++)
+    for(int i=1;i<Nt;i++)
     {
         for(int j=1;j<N-1;j++)
         {
             xi[2][j]=2*xi[1][j]-xi[0][j]+ratio*(xi[1][j+1]+xi[1][j-1]-2*xi[1][j]);
         }
-        
-        for(int j=0;j<N;j++)
-        {
-          outfile << xi[1][j] << " ";
-        }
-        outfile << "\n";    
-          
         for(int j=0;j<N;j++)
         {
             xi[0][j]=xi[1][j];
             xi[1][j]=xi[2][j];
-        }  
-                
+        }
+        for(int j=0;j<N;j++)
+        {
+            outfile << xi[1][j] << " ";
+        }
+        outfile << "\n";
     }
-    
     outfile.close();
 }
 
--- ploteo.py
+++ ploteo.py
@@ -1,29 +1,32 @@
-import numpy as np 
+import numpy as np
 import matplotlib.pyplot as plt
 import os
-from mpl_toolkits.mplot3d import Axes3D
 
-os.system("g++ c.cpp")
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+os.system("g++ correr.cpp")
 os.system("./a.out")
 
 pinocho = np.loadtxt("datos.dat")
 
-a=np.linspace(1,len(pinocho[0]),len(pinocho[0]))
+a=np.linspace(0,len(pinocho[0]),len(pinocho[0]))
 
-plt.subplot(121)
+
 
 fig = plt.figure()
 ax = Axes3D(fig)
-ax.imshow(pinocho)
-ax.colorbar()
+x, y = np.mgrid[0:pinocho.shape[0],0:pinocho.shape[1]]
+surfe=ax.plot_surface(x,y,pinocho,rstride=1,cstride=1,cmap=cm.coolwarm,linewidth=0,antialiased=False)
+fig.colorbar(surfe, shrink=0.5, aspect=5)
 plt.ylabel("Posicion [metros]")
 plt.xlabel("Tiempo [segundos]")
-
-plt.subplot(122)
-plt.plot(a,pinocho[0])
-plt.plot(a,pinocho[-1])
-plt.ylabel("Temperatura")
-plt.xlabel("Indice X")
-plt.legend()
-
 plt.savefig("graficas.png")
+
+plt.figure()
+plt.plot(a,pinocho[0],label="tiempo inicial")
+plt.plot(a,pinocho[-1],label="tiempo final")
+plt.legend()
+plt.xlabel("Posicion [metros]")
+plt.ylabel("Desplazamiento [metros]")
+plt.savefig("graficas2.png")
+
